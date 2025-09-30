dedb2aa5d0c1503590cc41f28058b2189083bf41
hyacinthe Goula <47952336+dagen2304@users.noreply.github.com>
2025-09-30 18:25:25 +0100

    Update DB.js

--- config/DB.js
+++ config/DB.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 const mongoose = require("mongoose");
 
- const connectionUrl = `mongodb+srv://${process.env.DB_USER_ID}:${process.env.DB_PASS}@todo-list.yqi65.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`
+ const connectionUrl = `mongodb+srv://${process.env.DB_USER_ID}:${process.env.DB_PASS}@ktc-cls.e8tgmud.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority&appName=ktc-cls`
 //const connectionUrl = `mongodb://localhost:27017/Parcinfo?retryWrites=true&w=majority`
 
 mongoose.connect(connectionUrl, {
